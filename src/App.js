3fb1ce1c127346f19786ddaf389f2e606105eaca
Nuccino92 <anthnucci@hotmail.com>
2021-11-16 23:14:23 -0500

    router paths added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Shop from "./components/shop/Shop";
+import Cart from "./components/cart/Cart";
+import Electronics from "./components/shop/Electronics";
+import Jewelery from "./components/shop/Jewelery";
+import MensClothing from "./components/shop/MensClothing";
+import WomensClothing from "./components/shop/WomensClothing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/electronics" element={<Electronics />} />
+          <Route path="/shop/jewelery" element={<Jewelery />} />
+          <Route path="/shop/men's%20clothing" element={<MensClothing />} />
+          <Route path="/shop/women's%20clothing" element={<WomensClothing />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
