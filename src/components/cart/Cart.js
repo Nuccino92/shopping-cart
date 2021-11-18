792e41976743202a35c0acbd4eb7b3dec7e9d7c1
Nuccino92 <anthnucci@hotmail.com>
2021-11-18 02:33:32 -0500

    set conditional classname, mapped over products to display cartitem component

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,25 @@
-const Cart = () => {
-  return <div>Cart</div>;
+import { Link } from "react-router-dom";
+import CartItem from "./CartItem";
+import uniqid from "uniqid";
+
+const Cart = (props) => {
+  const { active, cart, removeFromCart } = props;
+  return (
+    <div className={active ? "cartSticky active" : "cartSticky"}>
+      Cart
+      {cart.map((product) => {
+        return (
+          <CartItem
+            removeFromCart={removeFromCart}
+            product={product}
+            key={uniqid()}
+          />
+        );
+      })}
+      <Link to="/checkout">
+        <button>Proceed to Checkout</button>
+      </Link>
+    </div>
+  );
 };
 export default Cart;
