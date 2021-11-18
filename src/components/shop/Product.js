cac5309aefd133adaf62a1b713a6079f58b708a6
Nuccino92 <anthnucci@hotmail.com>
2021-11-18 02:40:05 -0500

    added onclick for addtocart. giving necessary paramaters

--- src/components/shop/Product.js
+++ src/components/shop/Product.js
@@ -1,6 +1,12 @@
 const Product = (props) => {
-  const { info } = props;
-  console.log(info);
-  return <div>{info.title}</div>;
+  const { info, addToCart } = props;
+  return (
+    <div>
+      {info.id} +{info.price}
+      <button onClick={() => addToCart(info.title, 1, info.price)}>
+        add to cart
+      </button>
+    </div>
+  );
 };
 export default Product;
