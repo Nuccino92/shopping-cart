cf22646ebb00a215d88580d073313e6ea37cfa01
Nuccino92 <anthnucci@hotmail.com>
2021-11-19 21:47:30 -0500

    added cart functions from App.js

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,10 +1,31 @@
 const CartItem = (props) => {
-  const { product, removeFromCart } = props;
+  const { product, removeFromCart, handleCartIncrements, handleInputField } =
+    props;
+
   return (
     <div>
       {product.title} --
-      {product.quantity} --
       {product.price}
+      <button
+        onClick={(e) =>
+          handleCartIncrements(product.title, product.quantity, e)
+        }
+      >
+        -
+      </button>
+      <input
+        id="cartInput"
+        onChange={(e) => handleInputField(product.title, product.quantity, e)}
+        type="number"
+        value={product.quantity}
+      ></input>
+      <button
+        onClick={(e) =>
+          handleCartIncrements(product.title, product.quantity, e)
+        }
+      >
+        +
+      </button>
       <div onClick={() => removeFromCart(product.title)}>X</div>
     </div>
   );
