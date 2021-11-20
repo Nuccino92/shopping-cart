4e4f05fbcac72b5df70b4c6ca04a5b79a568866f
Nuccino92 <anthnucci@hotmail.com>
2021-11-19 21:48:24 -0500

    cart icon conditions showing num of items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,40 @@
 import { Link } from "react-router-dom";
 
 const Header = (props) => {
+  let numItemsText = "";
   const { handleCartActive, numberOfItems } = props;
+
+  numberOfItems > 99 ? (numItemsText = "99+") : (numItemsText = numberOfItems);
+
   return (
     <header>
-      <nav>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/shop">
-          <li>Categories</li>
-        </Link>
-        <li onClick={() => handleCartActive()}>Cart {numberOfItems}</li>
-      </nav>
+      <div className="headerContentContainer">
+        <h1>shamazon</h1>
+        <nav>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/shop">
+            <li>Categories</li>
+          </Link>
+          <li className="cart-container" onClick={() => handleCartActive()}>
+            <img
+              className="shopping-cart-image"
+              src="../shopping-cart.png"
+              alt="shopping cart"
+            ></img>
+            <div
+              className={
+                numberOfItems > 0
+                  ? "total-items-container active"
+                  : "total-items-container"
+              }
+            >
+              {numItemsText}
+            </div>
+          </li>
+        </nav>
+      </div>
     </header>
   );
 };
