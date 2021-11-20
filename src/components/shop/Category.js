27bbe7cb658c5a3dc84d458b611e83abea8d0bb8
Nuccino92 <anthnucci@hotmail.com>
2021-11-19 21:50:33 -0500

    added category images w/ function to find correct img

--- src/components/shop/Category.js
+++ src/components/shop/Category.js
@@ -1,6 +1,21 @@
 const Category = (props) => {
   const { category } = props;
+  console.log(category);
 
-  return <div>{category}</div>;
+  const getId = (category) => {
+    if (category === "electronics") return "electronics-img";
+    if (category === "jewelery") return "jewelery-img";
+    if (category === `men's clothing`) return "mens-clothing-img";
+    if (category === `women's clothing`) return "womens-clothing-img";
+  };
+
+  return (
+    <div className="category-container">
+      <div className="category-img-container">
+        <div className="category-img" id={getId(category)}></div>
+      </div>
+      <h3>{category}</h3>
+    </div>
+  );
 };
 export default Category;
