9eef4142ce9b3639411d63b7bad216a9441b8f87
Nuccino92 <anthnucci@hotmail.com>
2021-11-20 23:39:11 -0500

    added divs for product display

--- src/components/shop/Product.js
+++ src/components/shop/Product.js
@@ -1,12 +1,27 @@
 const Product = (props) => {
   const { info, addToCart } = props;
+
+  const { image } = info;
+  const style = {
+    backgroundImage: `url(${image})`,
+  };
+
   return (
-    <div>
-      {info.id} +{info.price}
-      <button onClick={() => addToCart(info.title, 1, info.price)}>
-        add to cart
-      </button>
+    <div title={info.description} className="product">
+      <div className="product-container">
+        <div className="product-image-container">
+          <div style={style} className="product-image"></div>
+        </div>
+        <h3>{info.title}</h3>
+        <p> ${info.price}</p>
+        <button
+          onClick={() => addToCart(info.title, 1, info.price, info.image)}
+        >
+          add to cart
+        </button>
+      </div>
     </div>
   );
 };
+
 export default Product;
