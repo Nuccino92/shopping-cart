d03b7b7c0531458acfb14c98cfac5871659d135c
Nuccino92 <anthnucci@hotmail.com>
2021-11-20 23:38:10 -0500

    cart design

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,32 +1,43 @@
 const CartItem = (props) => {
   const { product, removeFromCart, handleCartIncrements, handleInputField } =
     props;
-
   return (
-    <div>
-      {product.title} --
-      {product.price}
-      <button
-        onClick={(e) =>
-          handleCartIncrements(product.title, product.quantity, e)
-        }
+    <div className="cart-items">
+      <div
+        className="cart-image-container"
+        style={{ backgroundImage: `url(${product.image})` }}
+      ></div>
+      <h3> {product.title}</h3>
+      <p>${product.price}</p>
+      <div className="cart-input-container">
+        <button
+          onClick={(e) =>
+            handleCartIncrements(product.title, product.quantity, e)
+          }
+        >
+          ←
+        </button>
+        <input
+          id="cartInput"
+          onChange={(e) => handleInputField(product.title, product.quantity, e)}
+          type="number"
+          value={product.quantity}
+        ></input>
+        <button
+          onClick={(e) =>
+            handleCartIncrements(product.title, product.quantity, e)
+          }
+        >
+          →
+        </button>
+      </div>
+      <div
+        className="remove-btn"
+        title="remove"
+        onClick={() => removeFromCart(product.title)}
       >
-        -
-      </button>
-      <input
-        id="cartInput"
-        onChange={(e) => handleInputField(product.title, product.quantity, e)}
-        type="number"
-        value={product.quantity}
-      ></input>
-      <button
-        onClick={(e) =>
-          handleCartIncrements(product.title, product.quantity, e)
-        }
-      >
-        +
-      </button>
-      <div onClick={() => removeFromCart(product.title)}>X</div>
+        X
+      </div>
     </div>
   );
 };
